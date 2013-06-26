be6c762a2ec06d878ece107f8c35ab9fb3637a42
Pascal Guilcher <guilcher.p@sfeir.com>
2013-06-26 13:43:10 +0200

    ..

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,10 @@ import com.google.appengine.tools.remoteapi.RemoteApiInstaller;
 import com.google.appengine.tools.remoteapi.RemoteApiOptions;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -29,12 +33,24 @@ public class Main {
         try {
             DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
 
-            Player player = new Player();
-            player.setText("Crash: it does not just happen to computers.");
-            player.setPictureUrl("http://24.media.tumblr.com/a1dfdda0494a2506a3fc4bb9184540f0/tumblr_mnryxtDBMu1rcufs7o1_1280.jpg");
-            dao.ofy().put(player);
+            Map<String, String> data = new HashMap<String, String>();
+            data.put("Networking is one letter from Not working", //
+                    "http://25.media.tumblr.com/f74d771d1fc640d6eb32d4f7242b4b71/tumblr_moca9xWmSv1rcufs7o1_1280.png");
+            data.put("Teamwork: Tackle life side by side.", //
+                    "http://24.media.tumblr.com/9eed7a0ca40b301569a67fe29e3d3cc6/tumblr_mnotrbV7Hz1rcufs7o1_1280.png");
+            data.put("Crash: it does not just happen to computers.", //
+                    "http://24.media.tumblr.com/a1dfdda0494a2506a3fc4bb9184540f0/tumblr_mnryxtDBMu1rcufs7o1_1280.jpg");
 
-            System.out.println("Player id: " + player.getId());
+            List<Player> players = new ArrayList<Player>();
+            for (Map.Entry<String, String> e : data.entrySet()) {
+                Player p = new Player();
+                p.setText(e.getKey());
+                p.setPictureUrl(e.getValue());
+                players.add(p);
+            }
+            dao.ofy().put(players);
+
+            System.out.println("Done!");
 //            System.out.println("Key of new entity is " +
 //                    ds.put(new Entity("Hello Remote API!")));
         } finally {
